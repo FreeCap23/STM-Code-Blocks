b0ccd4a441f8732b9bf8e1249d7cf8f5d2c3328f
CiprianAtanasoaei <CiprianAtanasoaei@users.noreply.github.com>
2025-07-18 12:22:47 +0000

    deploy: 219c33740b976549a3ba1274d2a197ed4706ce1f

--- search/all_0.js
+++ search/all_0.js
@@ -1,22 +1,23 @@
 var searchData=
 [
   ['a_20global_20variables_20declaration_20definition_20and_20inclusion_20in_20the_20main_20source_20code_0',['A. Global Variables - Declaration, definition and inclusion in the main source code',['../md_Guides_2Arduino__to__STM.html#autotoc_md2',1,'']]],
-  ['a_20timer_20interrupt_1',['Setting a timer interrupt',['../md_Guides_2Peripheral__Setup.html#autotoc_md15',1,'']]],
+  ['a_20timer_20interrupt_1',['Setting a timer interrupt',['../md_Guides_2Peripheral__Setup.html#autotoc_md16',1,'']]],
   ['a_20tt_20globals_20c_20tt_20file_20following_20these_20steps_3a_2',['The global variables you declared in &lt;tt&gt;globals.h&lt;/tt&gt; must be also defined (assigned default values) into a &lt;tt&gt;globals.c&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md4',1,'']]],
   ['a_20tt_20globals_20h_20tt_20file_20following_20these_20steps_3a_3',['Global variables should be declared in a &lt;tt&gt;globals.h&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md3',1,'']]],
   ['adc_20in_20dma_20mode_20effectively_4',['Using the ADC in DMA mode effectively',['../md_Guides_2Using__the__ADC__with__DMA.html',1,'']]],
   ['also_20defined_20assigned_20default_20values_20into_20a_20tt_20globals_20c_20tt_20file_20following_20these_20steps_3a_5',['The global variables you declared in &lt;tt&gt;globals.h&lt;/tt&gt; must be also defined (assigned default values) into a &lt;tt&gt;globals.c&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md4',1,'']]],
-  ['an_20interrupt_20on_20an_20arbitrary_20pin_6',['Triggering an interrupt on an arbitrary pin',['../md_Guides_2Peripheral__Setup.html#autotoc_md12',1,'']]],
-  ['analog_20input_7',['Analog Input',['../md_Guides_2Peripheral__Setup.html#autotoc_md10',1,'']]],
+  ['an_20interrupt_20on_20an_20arbitrary_20pin_6',['Triggering an interrupt on an arbitrary pin',['../md_Guides_2Peripheral__Setup.html#autotoc_md13',1,'']]],
+  ['analog_20input_7',['Analog Input',['../md_Guides_2Peripheral__Setup.html#autotoc_md11',1,'']]],
   ['analogwrite_8',['analogWrite',['../pwm__helper_8h.html#af1dae7be96dd1f02d2bbdd6caa495ade',1,'pwm_helper.h']]],
   ['analogwritepercent_9',['analogWritePercent',['../pwm__helper_8h.html#a6f8cb0886f2512a418b26cd41cd2dfe6',1,'pwm_helper.h']]],
-  ['and_20flash_20the_20project_10',['Build and Flash the Project',['../md_Guides_2Using__the__Debugger.html#autotoc_md23',1,'']]],
+  ['and_20flash_20the_20project_10',['Build and Flash the Project',['../md_Guides_2Using__the__Debugger.html#autotoc_md24',1,'']]],
   ['and_20inclusion_20in_20the_20main_20source_20code_11',['A. Global Variables - Declaration, definition and inclusion in the main source code',['../md_Guides_2Arduino__to__STM.html#autotoc_md2',1,'']]],
-  ['and_20loop_12',['What Is the Equivalent of setup() and loop()?',['../md_Guides_2Arduino__to__STM.html#autotoc_md7',1,'']]],
-  ['and_20registers_13',['Memory and Registers',['../md_Guides_2Using__the__Debugger.html#autotoc_md29',1,'']]],
-  ['apbx_20peripheral_20clocks_14',['APBx Peripheral Clocks',['../md_Guides_2Timer__Clock.html#autotoc_md17',1,'']]],
-  ['arbitrary_20pin_15',['Triggering an interrupt on an arbitrary pin',['../md_Guides_2Peripheral__Setup.html#autotoc_md12',1,'']]],
+  ['and_20loop_12',['What Is the Equivalent of setup() and loop()?',['../md_Guides_2Arduino__to__STM.html#autotoc_md8',1,'']]],
+  ['and_20registers_13',['Memory and Registers',['../md_Guides_2Using__the__Debugger.html#autotoc_md30',1,'']]],
+  ['apbx_20peripheral_20clocks_14',['APBx Peripheral Clocks',['../md_Guides_2Timer__Clock.html#autotoc_md18',1,'']]],
+  ['arbitrary_20pin_15',['Triggering an interrupt on an arbitrary pin',['../md_Guides_2Peripheral__Setup.html#autotoc_md13',1,'']]],
   ['arduino_20users_16',['STM32 HAL Project Structure for Arduino Users',['../md_Guides_2Arduino__to__STM.html',1,'']]],
   ['assigned_20default_20values_20into_20a_20tt_20globals_20c_20tt_20file_20following_20these_20steps_3a_17',['The global variables you declared in &lt;tt&gt;globals.h&lt;/tt&gt; must be also defined (assigned default values) into a &lt;tt&gt;globals.c&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md4',1,'']]],
-  ['available_20to_20the_20main_20code_20the_20tt_20globals_20h_20tt_20file_20must_20be_20included_20into_20tt_20main_20c_20tt_20file_20following_20these_20steps_3a_18',['To make you global variables available to the main code, the &lt;tt&gt;globals.h&lt;/tt&gt; file must be included into &lt;tt&gt;main.c&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md5',1,'']]]
+  ['available_20to_20other_20source_20code_20files_20the_20tt_20globals_20h_20tt_20file_20must_20be_20included_20into_20tt_20main_20c_20tt_20file_20following_20these_20steps_3a_18',['To make your global variables available to other source code files, the &lt;tt&gt;globals.h&lt;/tt&gt; file must be included into &lt;tt&gt;main.c&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md6',1,'']]],
+  ['available_20to_20the_20main_20code_20the_20tt_20globals_20h_20tt_20file_20must_20be_20included_20into_20tt_20main_20c_20tt_20file_20following_20these_20steps_3a_19',['To make your global variables available to the main code, the &lt;tt&gt;globals.h&lt;/tt&gt; file must be included into &lt;tt&gt;main.c&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md5',1,'']]]
 ];
--- search/all_b.js
+++ search/all_b.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['of_20setup_20and_20loop_0',['What Is the Equivalent of setup() and loop()?',['../md_Guides_2Arduino__to__STM.html#autotoc_md7',1,'']]],
-  ['on_20an_20arbitrary_20pin_1',['Triggering an interrupt on an arbitrary pin',['../md_Guides_2Peripheral__Setup.html#autotoc_md12',1,'']]],
-  ['on_20variable_20types_2',['Libraries Required Based on Variable Types',['../md_Guides_2Arduino__to__STM.html#autotoc_md6',1,'']]],
-  ['output_3',['output',['../md_Guides_2Peripheral__Setup.html#autotoc_md9',1,'Digital Input / Output'],['../md_Guides_2Peripheral__Setup.html#autotoc_md11',1,'PWM Output']]]
+  ['of_20setup_20and_20loop_0',['What Is the Equivalent of setup() and loop()?',['../md_Guides_2Arduino__to__STM.html#autotoc_md8',1,'']]],
+  ['on_20an_20arbitrary_20pin_1',['Triggering an interrupt on an arbitrary pin',['../md_Guides_2Peripheral__Setup.html#autotoc_md13',1,'']]],
+  ['on_20variable_20types_2',['Libraries Required Based on Variable Types',['../md_Guides_2Arduino__to__STM.html#autotoc_md7',1,'']]],
+  ['other_20source_20code_20files_20the_20tt_20globals_20h_20tt_20file_20must_20be_20included_20into_20tt_20main_20c_20tt_20file_20following_20these_20steps_3a_3',['To make your global variables available to other source code files, the &lt;tt&gt;globals.h&lt;/tt&gt; file must be included into &lt;tt&gt;main.c&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md6',1,'']]],
+  ['output_4',['output',['../md_Guides_2Peripheral__Setup.html#autotoc_md10',1,'Digital Input / Output'],['../md_Guides_2Peripheral__Setup.html#autotoc_md12',1,'PWM Output']]]
 ];
