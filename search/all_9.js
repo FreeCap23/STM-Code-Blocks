65a303a006ca6d9b281ee20efcd747a775bac0c4
CiprianAtanasoaei <CiprianAtanasoaei@users.noreply.github.com>
2025-07-18 12:32:03 +0000

    deploy: fb77ad9c13c34529ac753cd4b4b492b3f8192216

--- search/all_9.js
+++ search/all_9.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['loop_0',['What Is the Equivalent of setup() and loop()?',['../md_Guides_2Arduino__to__STM.html#autotoc_md8',1,'']]]
+  ['loop_20functions_20found_20in_20arduine_20ide_0',['B. What Is the Equivalent of setup() and loop() functions found in Arduine IDE?',['../md_Guides_2Arduino__to__STM.html#autotoc_md7',1,'']]]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['registers_0',['Memory and Registers',['../md_Guides_2Using__the__Debugger.html#autotoc_md30',1,'']]]
+  ['registers_0',['Memory and Registers',['../md_Guides_2Using__the__Debugger.html#autotoc_md29',1,'']]]
 ];
