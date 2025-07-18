9d52fd1c6b8dec23f08853e8d34bcf1af24d10a6
CiprianAtanasoaei <CiprianAtanasoaei@users.noreply.github.com>
2025-07-18 12:16:22 +0000

    deploy: 7628d18ae9d6a05d5e592fec38915284ee44f21e

--- search/all_2.js
+++ search/all_2.js
@@ -4,7 +4,6 @@ var searchData=
   ['clocks_1',['APBx Peripheral Clocks',['../md_Guides_2Timer__Clock.html#autotoc_md15',1,'']]],
   ['code_2',['Step Through Code',['../md_Guides_2Using__the__Debugger.html#autotoc_md23',1,'']]],
   ['code_20blocks_3',['STM Code Blocks',['../index.html',1,'']]],
-  ['code_20global_20variables_20should_20be_20declared_20in_20a_20tt_20globals_20h_20tt_20file_20following_20these_20steps_3a_4',['A. In STM32CubeMX-generated code, global variables should be declared in a &lt;tt&gt;globals.h&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md3',1,'']]],
-  ['command_5fparser_2eh_5',['command_parser.h',['../command__parser_8h.html',1,'']]],
-  ['communication_6',['communication',['../md_Guides_2Peripheral__Setup.html#autotoc_md12',1,'SPI Communication'],['../md_Guides_2Peripheral__Setup.html#autotoc_md11',1,'UART Communication']]]
+  ['command_5fparser_2eh_4',['command_parser.h',['../command__parser_8h.html',1,'']]],
+  ['communication_5',['communication',['../md_Guides_2Peripheral__Setup.html#autotoc_md12',1,'SPI Communication'],['../md_Guides_2Peripheral__Setup.html#autotoc_md11',1,'UART Communication']]]
 ];
--- search/all_f.js
+++ search/all_f.js
@@ -3,11 +3,11 @@ var searchData=
   ['the_20adc_20in_20dma_20mode_20effectively_0',['Using the ADC in DMA mode effectively',['../md_Guides_2Using__the__ADC__with__DMA.html',1,'']]],
   ['the_20equivalent_20of_20setup_20and_20loop_1',['What Is the Equivalent of setup() and loop()?',['../md_Guides_2Arduino__to__STM.html#autotoc_md5',1,'']]],
   ['the_20project_2',['Build and Flash the Project',['../md_Guides_2Using__the__Debugger.html#autotoc_md21',1,'']]],
-  ['these_20steps_3a_3',['A. In STM32CubeMX-generated code, global variables should be declared in a &lt;tt&gt;globals.h&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md3',1,'']]],
+  ['these_20steps_3a_3',['A1 Global variables should be declared in a &lt;tt&gt;globals.h&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md3',1,'']]],
   ['through_20code_4',['Step Through Code',['../md_Guides_2Using__the__Debugger.html#autotoc_md23',1,'']]],
   ['timer_20clock_20guide_5',['Timer Clock Guide',['../md_Guides_2Timer__Clock.html',1,'']]],
   ['timer_20interrupt_6',['Setting a timer interrupt',['../md_Guides_2Peripheral__Setup.html#autotoc_md13',1,'']]],
   ['triggering_20an_20interrupt_20on_20an_20arbitrary_20pin_7',['Triggering an interrupt on an arbitrary pin',['../md_Guides_2Peripheral__Setup.html#autotoc_md10',1,'']]],
-  ['tt_20globals_20h_20tt_20file_20following_20these_20steps_3a_8',['A. In STM32CubeMX-generated code, global variables should be declared in a &lt;tt&gt;globals.h&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md3',1,'']]],
+  ['tt_20globals_20h_20tt_20file_20following_20these_20steps_3a_8',['A1 Global variables should be declared in a &lt;tt&gt;globals.h&lt;/tt&gt; file following these steps:',['../md_Guides_2Arduino__to__STM.html#autotoc_md3',1,'']]],
   ['types_9',['Libraries Required Based on Variable Types',['../md_Guides_2Arduino__to__STM.html#autotoc_md4',1,'']]]
 ];
