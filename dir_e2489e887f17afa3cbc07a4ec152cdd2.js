c0f83240e8ea673e98b2da19d9629dee71521571
FreeCap23 <FreeCap23@users.noreply.github.com>
2025-07-18 13:01:43 +0000

    deploy: 8c6723eecfccf4cc30770dd7ce8049970f588a80

--- dir_e2489e887f17afa3cbc07a4ec152cdd2.js
+++ dir_e2489e887f17afa3cbc07a4ec152cdd2.js
@@ -5,5 +5,5 @@ var dir_e2489e887f17afa3cbc07a4ec152cdd2 =
     [ "main.h", "main_8h.html", "main_8h" ],
     [ "pwm_helper.h", "pwm__helper_8h.html", "pwm__helper_8h" ],
     [ "serial_helper.h", "serial__helper_8h.html", "serial__helper_8h" ],
-    [ "spi_helper.h", "spi__helper_8h_source.html", null ]
+    [ "spi_helper.h", "spi__helper_8h.html", "spi__helper_8h" ]
 ];
---
+++ spi__helper_8h.js
@@ -0,0 +1,7 @@
+var spi__helper_8h =
+[
+    [ "SPI_Helper_t", "structSPI__Helper__t.html", "structSPI__Helper__t" ],
+    [ "spi_helper_init", "spi__helper_8h.html#a664446571d582c55d7834badf85cd2ba", null ],
+    [ "spi_helper_read_register", "spi__helper_8h.html#a97a98e84e7b2e760406210a147a245f6", null ],
+    [ "spi_helper_write_register", "spi__helper_8h.html#a05ca47f4375ae9a7648860ace3ebefae", null ]
+];
