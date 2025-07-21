21481f14c62029bff708b5df8eb608c68143a76d
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-07-21 15:33:56 +0300

    Add LCD with I2C interface documentation

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,6 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "i2c_lcd.h"
+#include <string.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -40,12 +42,15 @@
 
 /* Private variables ---------------------------------------------------------*/
 
+I2C_HandleTypeDef hi2c1;
+
 SPI_HandleTypeDef hspi1;
 
 UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
-
+I2C_LCD_HandleTypeDef hlcd;
+char buffer[64];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -54,8 +59,8 @@ static void MPU_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_USART3_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
-
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -97,13 +102,27 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   MX_USART3_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+  hlcd.hi2c = &hi2c1;
+  hlcd.address = 78;
+  lcd_init(&hlcd);
+  lcd_clear(&hlcd);
+  lcd_puts(&hlcd, "Hello there");
+
+  lcd_gotoxy(&hlcd, 0, 1);
+  sprintf(buffer, "Float (%%.3f): %.3f", 3.1415926535);
+  lcd_puts(&hlcd, buffer);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	sprintf(buffer, "Tick: %lu", HAL_GetTick());
+	lcd_gotoxy(&hlcd, 0, 2);
+	lcd_puts(&hlcd, buffer);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -171,6 +190,54 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x00602173;
+  hi2c1.Init.OwnAddress1 = 64;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
@@ -301,7 +368,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
 /* USER CODE END 4 */
 
  /* MPU Configuration */
---
+++ Guides/LCD_with_I2C.md
@@ -0,0 +1,42 @@
+# LCD With I<sup>2</sup>C
+
+[This library](https://github.com/alixahedi/I2C-LCD-STM32) allows us to easily use an LCD through I<sup>2</sup>C. It supports displays with sizes **16x2, 16x4, 20x2, 20x4**.
+
+## Installation
+For installation instructions, [see the README.md](https://github.com/alixahedi/I2C-LCD-STM32?tab=readme-ov-file#installation).
+
+## Displaying a formatted string
+In order to display a formatted string, we first need to build the string in a buffer:
+```c
+char buffer[64];
+sprintf(buffer, "Pi: %.3f", 3.1415926535);
+lcd_puts(&hlcd, buffer);
+```
+
+This will display `Pi: 3.142`. Note how the number was automatically rounded. For more format specifiers, see [the Parameters section of the printf documentation](https://cplusplus.com/reference/cstdio/printf/).
+
+## How do I find the address??
+First, try addresses `0x20` and `0x4E`. If those don't work, try running this snippet of code:
+
+```c
+/* ----- >8 snip ----- */
+/* USER CODE BEGIN PV */
+I2C_LCD_HandleTypeDef hlcd;
+/* USER CODE END PV */
+/* ----- >8 snip ----- */
+
+/* ----- >8 snip ----- */
+/* USER CODE BEGIN 2 */
+hlcd.hi2c = &hi2c1; // Change this to your i2c handle
+char buffer[64];
+for (int8_t address = 0; address < 128; address++) {
+  // Convert the address to a string in hex format
+  sprintf(buffer, "Address: %#X", address);
+  hlcd.address = address;
+  lcd_init(&hlcd);
+  lcd_clear(&hlcd);
+  lcd_puts(&hlcd, buffer);
+}
+/* ----- >8 snip ----- */
+```
+You should see the address printed on your display.
