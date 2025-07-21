5abe1e7f627da60d030d08528c90e0280ec543bc
CiprianAtanasoaei <CiprianAtanasoaei@users.noreply.github.com>
2025-07-21 12:21:25 +0000

    deploy: 2317c849c84c9c899b9e278a7eb7c4d89b582698

--- search/all_9.js
+++ search/all_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['loop_20functions_20found_20in_20arduine_20ide_0',['A. The STM32 equivalent of setup() and loop() functions found in Arduine IDE',['../md_Guides_201_8_01STM32__Code__Structure.html#autotoc_md2',1,'']]]
+  ['local_20variables_0',['D. Creating software module and its local variables',['../md_Guides_201_8_01STM32__Code__Structure.html#autotoc_md8',1,'']]],
+  ['loop_20functions_20found_20in_20arduine_20ide_1',['B. The STM32 equivalent of setup() and loop() functions found in Arduine IDE',['../md_Guides_201_8_01STM32__Code__Structure.html#autotoc_md3',1,'']]]
 ];
