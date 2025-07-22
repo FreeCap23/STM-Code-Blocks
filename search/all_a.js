9899d991bf17141420269f6caf1e221f584947d5
CiprianAtanasoaei <CiprianAtanasoaei@users.noreply.github.com>
2025-07-22 12:35:30 +0000

    deploy: 2dbe42ea6d16f64ae746800b990a0b9b54769cf4

--- search/all_a.js
+++ search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['lcd_20with_20i2c_0',['LCD With I2C',['../md_Guides_2LCD__with__I2C.html',1,'']]],
-  ['local_20variables_1',['D. Creating software module and its local variables',['../md_Guides_201_8_01STM32__Code__Structure.html#autotoc_md8',1,'']]],
-  ['loop_20functions_20found_20in_20arduine_20ide_2',['B. The STM32 equivalent of setup() and loop() functions found in Arduine IDE',['../md_Guides_201_8_01STM32__Code__Structure.html#autotoc_md3',1,'']]]
+  ['local_20variables_1',['D. Creating software module and its local variables',['../md_Guides_201_8STM32__Code__Structure.html#autotoc_md8',1,'']]],
+  ['loop_20functions_20found_20in_20arduine_20ide_2',['B. The STM32 equivalent of setup() and loop() functions found in Arduine IDE',['../md_Guides_201_8STM32__Code__Structure.html#autotoc_md3',1,'']]]
 ];
