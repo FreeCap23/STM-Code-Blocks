e64da9a55bfe446629b15712c24f933e89130141
CiprianAtanasoaei <CiprianAtanasoaei@users.noreply.github.com>
2025-07-22 12:08:11 +0000

    deploy: 8b604ff0ec2f0a9a11749f6408e35fa44fccaff0

--- search/pages_b.js
+++ search/pages_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['the_20adc_20in_20dma_20mode_20effectively_0',['Using the ADC in DMA mode effectively',['../md_Guides_2Using__the__ADC__with__DMA.html',1,'']]],
-  ['the_20stm32_20code_20structure_1',['The STM32 Code Structure',['../md_Guides_201_8_01STM32__Code__Structure.html',1,'']]],
+  ['the_20stm32_20code_20strucrure_1',['The STM32 Code Strucrure',['../md_Guides_201_8_01STM32__Code__Structure.html',1,'']]],
   ['timer_20clock_20guide_2',['Timer Clock Guide',['../md_Guides_2Timer__Clock.html',1,'']]]
 ];
