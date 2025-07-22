5f0b8362df708c450137d7739231c301a0bffec7
CiprianAtanasoaei <CiprianAtanasoaei@users.noreply.github.com>
2025-07-22 12:34:07 +0000

    deploy: ac01cf0e90057bdc5bc1d7ba873bcb28a18677a5

--- search/all_11.js
+++ search/all_11.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['uart_20communication_0',['UART Communication',['../md_Guides_202_8Device_01Configuration__Setup__Guide.html#autotoc_md18',1,'']]],
+  ['uart_20communication_0',['UART Communication',['../md_Guides_202_8Device__Configuration__Setup.html#autotoc_md18',1,'']]],
   ['usage_1',['Example usage',['../md_Guides_2Using__the__ADC__with__DMA.html#autotoc_md30',1,'']]],
   ['using_20the_20adc_20in_20dma_20mode_20effectively_2',['Using the ADC in DMA mode effectively',['../md_Guides_2Using__the__ADC__with__DMA.html',1,'']]]
 ];
--- search/pages_a.js
+++ search/pages_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['setup_20guide_0',['02. Device Configuration Setup Guide',['../md_Guides_202_8Device_01Configuration__Setup__Guide.html',1,'']]],
+  ['setup_20guide_0',['02. Device Configuration Setup Guide',['../md_Guides_202_8Device__Configuration__Setup.html',1,'']]],
   ['stm_20code_20blocks_1',['STM Code Blocks',['../index.html',1,'']]],
   ['stm32_20code_20structure_2',['The STM32 Code Structure',['../md_Guides_201_8_01STM32__Code__Structure.html',1,'']]],
   ['stm32cubeide_20debugging_20guide_3',['STM32CubeIDE Debugging Guide',['../md_Guides_2Using__the__Debugger.html',1,'']]],
